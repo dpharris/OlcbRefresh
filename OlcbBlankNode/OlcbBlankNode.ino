3b60eed9000fe57afe6dc10526235e9615cb8108
David Harris <drdpharris@gmail.com>
2017-12-30 21:08:43 -0800

    Mocked OlcbBlankNode with OpenLCB class |

--- OlcbBlankNode/OlcbBlankNode.ino
+++ OlcbBlankNode/OlcbBlankNode.ino
@@ -1,12 +1,8 @@
 //==============================================================
 // OlcbBlnkNode
 //   A prototype of a basic 4-channel OpenLCB board
-// 
-//   setup() determines which are consumers and
-//   which are producers
 //
-//   Bob Jacobsen 2010, 2012
-//      based on examples by Alex Shepherd and David Harris
+//   David P Harris copyright 2017
 //==============================================================
 
 #define NUM_EVENT 3
@@ -15,27 +11,13 @@
 
 NodeID nodeid(5,1,1,1,3,255);    // This node's default ID; must be valid 
 
-
-
 // CDI
 //   Configuration Description Information in xml, must match MemStruct below
 //   See: http://openlcb.com/wp-content/uploads/2016/02/S-9.7.4.1-ConfigurationDescriptionInformation-2016-02-06.pdf
-extern "C" { const char configDefInfo[] PROGMEM = R"(
- <cdi xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:noNamespaceSchemaLocation='http://openlcb.org/trunk/prototypes/xml/schema/cdi.xsd'>
-    <identification>
-      <manufacturer>OpenLCB</manufacturer>
-      <model>OlcbBasicNode</model>
-      <hardwareVersion>1.0</hardwareVersion>
-      <softwareVersion>0.4</softwareVersion>
-    </identification>
-    <segment origin='12' space='253'> <!-- bypasses magic, nextEID, nodeID -->
-      <group>
-        <name>Node ID</name>
-        <description>User-provided description of the node</description>
-        <string size='20'><name>Node Name</name></string>
-        <string size='24'><name>Node Description</name></string>
-      </group>
-    <!-- ===== Enter User definitions below ===== -->
+extern "C" { 
+  const char configDefInfo[] PROGMEM = 
+      CDIheader R"(
+    // ===== Enter User definitions below =====
       <group replication='1'>
         <name>I/O Events</name>
         <description>Define events associated with input and output pins</description>
@@ -43,20 +25,8 @@ extern "C" { const char configDefInfo[] PROGMEM = R"(
         <eventid><name>Producer Event</name></eventid>
         <eventid><name>P/C Event</name></eventid>
       </group>
-    <!-- ===== Enter User definitions above ===== -->
-    </segment>
-    <segment origin='0' space='253'> <!-- stuff magic to trigger resets -->
-      <name>Reset</name>
-      <description>Controls reloading and clearing node memory. Board must be restarted for this to take effect.</description>
-      <int size='4'>
-        <map>
-          <relation><property>3998572261</property><value>(No reset)</value></relation>
-          <relation><property>3998561228</property><value>User clear: New default EventIDs, blank strings</value></relation>
-          <relation><property>0</property><value>Mfg clear: Reset all, including Node ID</value></relation>
-        </map>
-      </int>
-    </segment>
-  </cdi>)";
+    // ===== Enter User definitions above =====
+      )"CDIfooter;
 }
 
 // ===== MemStruct =====
@@ -64,35 +34,36 @@ extern "C" { const char configDefInfo[] PROGMEM = R"(
 typedef struct { 
     NodeVar nodeVar;         // must remain
     // ===== Enter User definitions below =====
-    EventID ceid;              // example consumer eventID
-    EventID peid;              // example producer eventID
-    EventID pceid;             // example prodcuder/consumer eventID
+      struct {
+        EventID ceid;              // example consumer eventID
+        EventID peid;              // example producer eventID
+        EventID pceid;             // example prodcuder/consumer eventID
+      } io;
     // ===== Enter User definitions above =====
   } MemStruct;                 // type definition
-MemStruct * pmem = 0;   
-// ===== eventidOffset
+MemStruct *pmem = 0;   
+
+// ===== eventidOffset =====
 // Array of offsets of every eventID in MemStruct/EEPROM/mem, and P/C flags
 const PROGMEM EIDTab eventidOffset[] = {
-      CEID(ceid),    // installs offset to a consumer-eventID with its flags
-      PEID(peid),    // installs offset to a producer-eventID with its flags 
-      PCEID(pceid)   // installs offset to an eventID that is both a producer and a consumer, with its flags
+      CEID(io.ceid),    // installs offset to a consumer-eventID with its flags
+      PEID(io.peid),    // installs offset to a producer-eventID with its flags 
+      PCEID(io.pceid)   // installs offset to an eventID that is both a producer and a consumer, with its flags
   };
 
-// next lines get "warning: only initialized variables can be placed into program memory area" due to GCC bug
-extern "C" {
-  
+extern "C" {  
     // SNIP Short node description for use by the Simple Node Information Protocol 
     //  See: http://openlcb.com/wp-content/uploads/2016/02/S-9.7.4.3-SimpleNodeInformation-2016-02-06.pdf
-    const char SNII_const_data[] PROGMEM = "\001OpenLCB\000DPHOlcbBasicNode\0001.0\000" OlcbCommonVersion ; // last zero in double-quote
+    const char SNII_const_data[] PROGMEM = 
+        "\001OpenLCB\000DPHOlcbBlankNode\0001.0\000" OlcbCommonVersion ; // last zero in double-quote
 
     // PIP Protocol Identification Protocol uses a bit-field to indicate which protocols this node supports
     //  See 3.3.6 and 3.3.7 in http://openlcb.com/wp-content/uploads/2016/02/S-9.7.3-MessageNetwork-2016-02-06.pdf
     const uint8_t protocolIdentValue[6] PROGMEM= {
-      pSimple | pDatagram | pMemConfig | pPCEvents | pIdent | pTeach, // 1st byte
-      pACDI | pSNIP | pCDI,                                           // 2nd byte
-      0, 0, 0, 0                                                      // remaining bytes
+        pSimple | pDatagram | pMemConfig | pPCEvents | pIdent | pTeach, // 1st byte
+        pACDI | pSNIP | pCDI,                                           // 2nd byte
+        0, 0, 0, 0                                                      // remaining bytes
     };
-
 }; // end extern "C"
 
 // Optional definition of Status indicators and/or buttons
@@ -100,9 +71,6 @@ extern "C" {
 #define GOLD 19    //
 ButtonLed blue(BLUE, LOW);
 ButtonLed gold(GOLD, LOW);
-
-OpenLCB olcb( &nodeid, NUM_EVENT, event, eventsIndex, eventidOffset );
-//Nodal_t nodal = { &NodeID(5,1,1,1,3,255), events, eventsIndex, eventidOffset, NUM_EVENT };  // alternate form.
 
 // ===== I/O Definitions ======
 // input/output pin drivers
@@ -131,29 +99,21 @@ void pceCallback(uint16_t index){
 
 // ===== Process Producers-eventIDs =====
 bool getState(unsigned p){}
-void produceFromInputs() {  
-  static bool state[] = {false, false, false}; 
-  for(unsigned p; p<NUM_EVENT; p++) {
-    if(event[p].flags & Event::CAN_PRODUCE_FLAG)
-      if( getState(p) != state[p]) {
-        state[p] ^= 1;  // flip state
-        event[p].send(); 
-      }
-  }
-}
+void produceFromInputs() { }
 
 // ===== Process changes to Configuration =====
-void userConfigWrite(unsigned int address, unsigned int length) {
-  if(length==0xFFFF) restore();
-}
+void configWritten(unsigned int address, unsigned int length) { }
 
+// Define OpenLCB instantiation
+OpenLCB olcb( &nodeid, eventidOffset, sizeof(MemStruct), pceCallback, configWritten);
 
 void setup() {
-    Olcb_setup();  // setup system
+    olcb.setup();  // setup system
+    // olcb.reset(); // uncomment to restore factory settings
 }
 
 void loop() {
-    bool activity = Olcb_loop();    // Process system
+    bool activity = olcb.loop();    // Process system
     if (activity) blue.blink(0x1);  // blink blue when a frame was received
     if (olcb.can_active) { 
         gold.blink(0x1);            // blink gold when a frame sent
--- OlcbBlankNode/OpenLCB.h
+++ OlcbBlankNode/OpenLCB.h
@@ -1,3 +1,34 @@
+#include <EEPROM.h>
+
+#define CDIheader R"( \
+ <cdi xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:noNamespaceSchemaLocation='http://openlcb.org/trunk/prototypes/xml/schema/cdi.xsd'> \
+    <identification> \
+      <manufacturer>OpenLCB</manufacturer>\
+      <model>OlcbBasicNode</model>\
+      <hardwareVersion>1.0</hardwareVersion>\
+      <softwareVersion>0.4</softwareVersion>\
+    </identification>\
+    <segment origin='12' space='253'> <!-- bypasses magic, nextEID, nodeID -->\
+      <group>\
+        <name>Node ID</name>\
+        <description>User-provided description of the node</description>\
+        <string size='20'><name>Node Name</name></string>\
+        <string size='24'><name>Node Description</name></string>\
+      </group>)"
+#define CDIfooter R"(\
+    </segment>\
+    <segment origin='0' space='253'> <!-- stuff magic to trigger resets -->\
+      <name>Reset</name>\
+      <description>Controls reloading and clearing node memory. Board must be restarted for this to take effect.</description>\
+      <int size='4'>\
+        <map>\
+          <relation><property>3998572261</property><value>(No reset)</value></relation>\
+          <relation><property>3998561228</property><value>User clear: New default EventIDs, blank strings</value></relation>\
+          <relation><property>0</property><value>Mfg clear: Reset all, including Node ID</value></relation>\
+        </map>\
+      </int>\
+    </segment>\
+  </cdi>)"
 
 typedef struct {
   uint32_t magic;         // used to check eeprom status
@@ -39,7 +70,7 @@ typedef struct {
 #define pFirmwareUpgrade     0x20
 #define pFirwareUpdateActive 0x10
 
-#define OlcbCommonVersion "0.1"
+#define OlcbCommonVersion "0.0.1"
 class NodeID {
   public:
     uint8_t val[6];
@@ -49,16 +80,15 @@ class EventID {
   public:
     uint8_t val[6];
     EventID(uint8_t a,uint8_t b,uint8_t c,uint8_t d,uint8_t e,uint8_t f,uint8_t g, uint8_t h){}
+    EventID(){}
 };
 #define PRODUCE_FLAG 0x02
 class Event {
   public:
     uint16_t flags;
     void send() { flags |= PRODUCE_FLAG; }
-  // Mark entry as consumer
-  static const int CAN_CONSUME_FLAG = 0x20;
-  // Mark entry as producer
-  static const int CAN_PRODUCE_FLAG = 0x40;
+    static const int CAN_CONSUME_FLAG = 0x20;
+    static const int CAN_PRODUCE_FLAG = 0x40;
 };
 class ButtonLed {
   public:
@@ -67,20 +97,81 @@ class ButtonLed {
     void on(uint32_t p){}
     void blink(uint32_t p){}
 };
+//Event event[NUM_EVENT] = { Event() };
+uint16_t eventsIndex[NUM_EVENT];  // Sorted index to eventids
+NodeVar* pnv = 0;
+#define NV(x) ((unsigned int)&pnv->x)
+
 class OpenLCB {
   public:
+    NodeID* nid;
+    uint16_t nextEID = 0;
+    uint16_t nevent = NUM_EVENT;
+    EventID eventid[NUM_EVENT];
+    Event event[NUM_EVENT];
+    uint16_t index[NUM_EVENT];
+    EIDTab* eidOffset;
+    uint16_t sMemStruct;
     bool can_active;
-    OpenLCB( NodeID* nodeid, uint16_t nevent, Event* event, uint16_t* eventIndex, const EIDTab* eventidOffset ){}
+    void (*pceCb)(unsigned int index);
+    void (*configW)(unsigned int adress, unsigned int length);
+    
+    OpenLCB( NodeID* _nodeid, const EIDTab* eventidOffset, uint16_t _sMemStruct,
+             void (*_pceCb)(unsigned int i), 
+             void (*_configW)(unsigned int a, unsigned int l) ) {
+        nid = _nodeid;
+        eidOffset = eventidOffset;
+        sMemStruct = _sMemStruct;
+        pceCb = _pceCb;
+        configW = _configW;
+    }
+    setup(){}
+    loop(){}
+    reset(){           // reset to factory
+      for(int i=0;i<sMemStruct;i++) EEPROM.write(i,0);  // clear EEPROM
+      EEPROM.put(NV(nid),nid);
+      for(int e=0;e<nevent;e++) {
+        EEPROM.put(eidOffset[e].offset,    nid);
+        EEPROM.write(eidOffset[e].offset,  nextEID>>8);
+        EEPROM.write(eidOffset[e].offset+1,nextEID);
+        nextEID++;
+      }
+      EEPROM.write(NV(nextEID),nextEID>>8);
+      EEPROM.write(NV(nextEID)+1,nextEID);
+    }
+    static int findCompare(const void* a, const void* b){
+       uint16_t ia = (uint16_t) a;
+       uint16_t ib = (uint16_t) b;
+       //for(int i=0; i<8; i++) {
+       //  if(eventid[ia].val[i]>eventid[ib].val[i]) return 1;
+       //  if(eventid[ia].val[i]<eventid[ib].val[i]) return -1;
+       //}
+       return 0; // they are equal
+    }
+    static int sortCompare(const void* a, const void* b){
+       uint16_t ia = (uint16_t) a;
+       uint16_t ib = (uint16_t) b;
+       //for(int i=0; i<8; i++) {
+       //  if(eventid[ia].val[i]>eventid[ib].val[i]) return 1;
+       //  if(eventid[ia].val[i]<eventid[ib].val[i]) return -1;
+       //}
+       return 0; // they are equal
+    }
+    initTables(){        // initialize tables
+      EEPROM.get(NV(nid),nid);
+      nextEID = EEPROM.read(NV(nextEID)) << 8;
+      nextEID += EEPROM.read(NV(nextEID+1));
+      for(int e=0; e<nevent; e++) {
+        index[e] = e;
+        EEPROM.get(eidOffset[e].offset, eventid[e]);
+        event[e].flags = eidOffset[e].flags;
+      }
+      for(int e=0; e<nevent; e++) 
+      qsort( index, NUM_EVENT, sizeof(index[0]), sortCompare);
+    }
 };
 
-Event event[NUM_EVENT] = { Event() };
-uint16_t eventsIndex[NUM_EVENT];  // Sorted index to eventids
-
-void Olcb_setup(){}  // setup system
-bool Olcb_loop(){}   // process system
-restore(){};        // restore tables
-
-// ===== User routines =====
-extern void pceCallback(uint16_t index);
-void userConfigWrite(unsigned int address, unsigned int length);
+// Establish location of node Name and Node Decsription in memory
+#define SNII_var_data &pmem->nodeName           // location of SNII_var_data EEPROM, and address of nodeName
+#define SNII_var_offset sizeof(pmem->nodeName)  // location of nodeDesc
 
